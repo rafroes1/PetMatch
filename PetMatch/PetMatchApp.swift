97f2528d25cd368817742bbc7657fd7a7f354358
rafroes1 <rafafroes@outlook.com>
2023-06-19 10:17:49 -0300

    Initial Commit

---
+++ PetMatch/PetMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetMatchApp.swift
+//  PetMatch
+//
+//  Created by Rafael Carvalho on 19/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PetMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
